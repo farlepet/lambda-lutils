f8d013974def34cfd7cb982925aa2b317117487c
Peter Farley <far.peter1@gmail.com>
2018-06-19 15:03:44 -0500

    `lshell` now displays MOTD and has a basis for input handling.

--- lshell/src/main.c
+++ lshell/src/main.c
@@ -1,12 +1,51 @@
-#include <lambda/sys/ktasks/kvid.h>
-#include <lambda/sys/syscalls.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
 
-int main() {
-	sys_kvid_init();
+#define LSHELL_VERSION "v0.0.1"
 
-    sys_kvid_print("\nlshell built "__DATE__"\n");
+
+const char *prompt_txt = "lshell>";
+
+void show_motd();
+
+int main(int argc, char **argv) {
+    (void)argc; (void)argv;
+
+    show_motd();
+
+    puts("lshell "LSHELL_VERSION", built "__DATE__"\n\n");
     
+    char cmd[256];
+
+    puts(prompt_txt);
+    while(1) {
+        gets(cmd);
+        if(strlen(cmd) > 0) {
+            printf("\nCommand: [%s]\n\n", cmd);
+            puts(prompt_txt);
+        }
+    }
+
     for(;;);
 
     return 0;
+}
+
+#define MOTD_BUFF_LEN 32
+
+void show_motd() {
+    int fd = open("/etc/motd", O_RDONLY);
+    if(fd < 0) return;
+
+    char motd_buff[MOTD_BUFF_LEN];
+    int  len;
+
+    while((len = read(fd, motd_buff, MOTD_BUFF_LEN)) > 0) {
+        write(STDOUT_FILENO, motd_buff, len);
+    }
+
+    putchar('\n');
+    close(fd);
 }
