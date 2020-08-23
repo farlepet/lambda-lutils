a4582b9ab954774d73b1a210f847f97e7762ed42
Peter Farley <far.peter1@gmail.com>
2020-08-23 15:39:01 -0500

    lshell: Can now execute external executables using `execve`.

--- lshell/src/shell.c
+++ lshell/src/shell.c
@@ -1,9 +1,18 @@
-#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
+#include <stdio.h>
+
+#include <sys/wait.h>
 
 #include <shell.h>
 
 const char *prompt_txt = SHELL_PROMPT_STR;
+
+static char *envp[] = {
+    "PATH=/",
+    NULL
+};
 
 static void handle_command(char *cmd);
 
@@ -12,6 +21,7 @@ void shell_loop(void) {
 
     puts(prompt_txt);
     while(1) {
+        // TODO: Replace gets, as it is unsafe
         gets(cmd);
         if(strlen(cmd) > 0) {
             handle_command(cmd);
@@ -57,21 +67,49 @@ static char **split_command(char *str) {
     return params;
 }
 
-static void handle_command(char *str) {
-    char **split = split_command(str);
-
-    char *cmd = split[0];
+static int execute_command(char **args) {
+    char *cmd = args[0];
 
     if(cmd == NULL) {
         printf("ERR: No command found.\n");
-        return;
+        return 1;
     }
 
     printf("Command: [%s]\n", cmd);
-    while(++split) {
-        if(*split == 0) break;
-        printf("  - Argument: [%s]\n", *split);
+    while(++args) {
+        if(*args == 0) break;
+        printf("  - Argument: [%s]\n", *args);
     }
 
-    // TODO: Actually handle command
+    // TODO: Check that command exists, and check all elements in PATH
+
+    pid_t pid = fork();
+
+    if(pid == -1) {
+        printf("Error while forking!\n");
+        return 1;
+    } else if(pid == 0) { // Child process
+        execve(cmd, args, envp);
+
+        printf("`execve` returned, something went wrong!\n");
+        //return 1;
+        exit(1);
+    } else { // Parent process
+        // TODO: Wait on child, then return return value
+        printf("Waiting for child (%d) to exit...\n", pid);
+        pid_t child = wait(NULL);
+        if(child == -1) {
+            printf("Something went wrong while waiting.\n");
+        } else {
+            printf("Child (%d) exited.\n", child);
+        }
+    }
+
+    return 0;
+}
+
+static void handle_command(char *str) {
+    char **split = split_command(str);
+
+    execute_command(split);
 }
